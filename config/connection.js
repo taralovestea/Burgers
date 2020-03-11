1c13c927e5126da298c41640cc92ce3d997af3cc
taralovestea <59029999+taralovestea@users.noreply.github.com>
2020-03-11 13:48:12 -0500

    fixed connection

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+require("dotenv").config();
+var connection;
+if (process.env.JAWSDB_URL){
+ connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else {
 
-var connection = mysql.createConnection({
+ connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "",
   database: "burgers_db2"
 });
-
+}
 // Make connection.
 connection.connect(function(err) {
   if (err) {
